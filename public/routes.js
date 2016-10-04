1642951cbb0934f7d3c8337590e8d77ad0af8c49
Vlad <vieira83@gmail.com>
2016-10-04 15:16:07 -0400

    updated titles services

--- public/routes.js
+++ public/routes.js
@@ -117,18 +117,19 @@ class Services extends Component {
     return (<div>
         <Grid>
           <Row>
+            <h2 className="porfolio-services-title">Services</h2>
             <Col sm={4}>
-              <h2>Front End Developement</h2>
+              <h3 className="porfolio-services-col-title">Front End Developement</h3>
               <p>Adipisicing ratione incidunt eaque expedita rerum porro inventore. Nihil sit ipsam iure officiis sit eos at quibusdam natus dignissimos natus dolore! Vel doloremque ipsa alias nihil harum laborum necessitatibus rerum?</p>
               <p><Button>View details »</Button></p>
             </Col>
             <Col sm={4}>
-              <h2>Backend Development</h2>
+              <h3 className="porfolio-services-col-title">Backend Development</h3>
               <p>Sit quia nemo quis enim provident porro eaque accusamus tenetur provident aliquid commodi? Velit nesciunt maiores obcaecati totam praesentium sint vitae exercitationem quaerat maxime iusto et! Consequatur aspernatur sit impedit.</p>
               <p><Button>View details »</Button></p>
             </Col>
             <Col sm={4}>
-              <h2>Web Desgin</h2>
+              <h3 className="porfolio-services-col-title">Web Desgin</h3>
               <p>Dolor aliquid dolores perferendis repellendus cum! Quam maiores blanditiis cupiditate voluptatibus voluptas aliquid nisi placeat tempora. Rem debitis accusamus pariatur officia corrupti. Architecto fuga reiciendis quos rem hic? Suscipit dignissimos.</p>
               <p><Button>View details »</Button></p>
             </Col>
