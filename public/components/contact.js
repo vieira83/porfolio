8a69e4d96c5a472b2232d936d0efe54f90e22ab2
Vlad <vieira83@gmail.com>
2016-10-05 17:18:34 -0400

    Added porfolio page structure

--- public/components/contact.js
+++ public/components/contact.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+// import Grid from 'react-bootstrap/lib/Grid';
+// import Row from 'react-bootstrap/lib/Row';
+import {Button,Col, Row, Grid, FormGroup, FormControl, ControlLabel} from 'react-bootstrap/lib';
+
+function FieldGroup (props ) {
+  var {id,label, help, ...props} = props;
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
 export class Contact extends Component {
   render(){
@@ -15,27 +26,33 @@ export class Contact extends Component {
                     MAP
                   </span>
               </div>
-
             </Col>
             <Col sm={5}>
               <div className="porfolio-contact-form-container">
                   <div className="porfolio-contact-form">
-
-                    <form method="post" enctype="multipart/form-data" action="/contact/">
-                      <ul>
-                        <li>
-                          <label class="gfield_label" for="input_2_1">Name<span class="gfield_required">*</span></label><input name="input_1" id="input_2_1" type="text" value="" class="medium" tabindex="1"/>
-                        </li>
-                        <li>
-                          <label class="gfield_label" for="input_2_1">Email<span class="gfield_required">*</span></label><input name="input_4" id="input_2_4" type="email" value="" class="medium" tabindex="2"/>
-                        </li>
-                      </ul>
-                      <textarea name="input_3" id="input_2_3" class="textarea medium" tabindex="4" rows="10" cols="50"></textarea>
-                      <input type="submit" id="gform_submit_button_2"/>
-                    </form>
+                    <form>
+                       <FieldGroup
+                         id={"formControlsText"}
+                         type={"text"}
+                         label="Name"
+                         placeholder="Enter text"
+                       />
+                       <FieldGroup
+                         id={"formControlsText"}
+                         type={"email"}
+                         label="Email"
+                         placeholder="Enter Email"
+                       />
+                       <FormGroup controlId="formControlsTextarea">
+                         <ControlLabel>Textarea</ControlLabel>
+                         <FormControl componentClass="textarea" placeholder="textarea" />
+                       </FormGroup>
+                       <Button type="submit">
+                         Submit
+                       </Button>
+                      </form>
                   </div>
               </div>
-
             </Col>
           </Row>
         </Grid>
--- public/components/porfolio.js
+++ public/components/porfolio.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import {Button, Col, Row, Grid} from 'react-bootstrap/lib';
 
 export class Porfolio extends Component {
   render(){
     return (<div className="porfolio-section porfolio">
-        <h2>Work</h2>
-        <div>Some Porfolio page content here</div>
-      </div>
+          <Grid>
+            <Row>
+              <h1 className="porfolio-title">Porfolio</h1>
+              <Col sm={6}>
+                <div className="col-main-background about">
+                    <span className="porfolio-about-image"></span>
+                    <div className="porfolio-porfolio-description">
+                      <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                    </div>
+                </div>
+              </Col>
+              <Col sm={6}>
+                <div className="col-main-background about">
+                    <span className="porfolio-about-image"></span>
+                    <div className="porfolio-porfolio-description">
+                      <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                    </div>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+            <Col sm={6}>
+              <div className="col-main-background about">
+                  <span className="porfolio-about-image"></span>
+                  <div className="porfolio-porfolio-description">
+                    <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                  </div>
+              </div>
+            </Col>
+            <Col sm={6}>
+              <div className="col-main-background about">
+                  <span className="porfolio-about-image"></span>
+                  <div className="porfolio-porfolio-description">
+                    <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                  </div>
+              </div>
+            </Col>
+            </Row>
+            <Row>
+            <Col sm={6}>
+              <div className="col-main-background about">
+                  <span className="porfolio-about-image"></span>
+                  <div className="porfolio-porfolio-description">
+                    <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                  </div>
+              </div>
+            </Col>
+            <Col sm={6}>
+              <div className="col-main-background about">
+                  <span className="porfolio-about-image"></span>
+                  <div className="porfolio-porfolio-description">
+                    <p>I have over 2 years of experience working in web development, I have experience working in front-end and back-end development. I have worked with different tools and languages such as Wordpress, HTML/CSS, javascript, Backbone.js, jQuery, PHP, Python, Django, MongoDB, etc, to create responsive and mobile ready web sites/App. </p>
+                  </div>
+              </div>
+            </Col>
+            </Row>
+          </Grid>
+        </div>
     )
   }
 }
